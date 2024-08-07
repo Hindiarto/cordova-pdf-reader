a58f87f073a7cd96ce42ced0f6872458d087b858
hindi <albertushindi@primamasterbank.co.id>
2024-08-07 08:36:52 +0700

    add closed button

--- src/android/PDFViewerActivity.java
+++ src/android/PDFViewerActivity.java
@@ -1,12 +1,12 @@
 package com.hindicorp.pdfviewer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.*;
 import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
-import com.primamasterbank.absensikaryawan.R;
 
 import android.view.View;
 import android.widget.Button;
@@ -17,11 +17,17 @@ public class PDFViewerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pdfviewer);
+
+        Context context = getApplicationContext();
+        int layoutResID = context.getResources().getIdentifier("activity_pdfviewer", "layout", context.getPackageName());
+        int closeButtonId = context.getResources().getIdentifier("closeButton", "id", context.getPackageName());
+        int pdfViewId = context.getResources().getIdentifier("pdfView", "id", context.getPackageName());
+
+        setContentView(layoutResID);
 
 
-        PDFView pdfView = findViewById(R.id.pdfView);
-        Button closeButton = findViewById(R.id.closeButton);
+        PDFView pdfView = findViewById(pdfViewId);
+        Button closeButton = findViewById(closeButtonId);
 
         //PDFView pdfView = new PDFView(this, null);
         //setContentView(pdfView);
